29cebf9c269da91cdb525c55a1070d1ea8dd835c
Solodovnikova Ekaterina <colo18@yandex.ru>
2019-10-19 16:52:59 +0500

    fixup! changed path to results

--- server.js
+++ server.js
@@ -124,22 +124,23 @@ app.post('/add', (req, res) => {
 
 app.get('/files', (_res, req) => {
     var filesList = [];
-    fs.readdir(__dirname+'/results', function (err, files) {
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
-        files.forEach(function (file) {
-            console.log(file)
-            if (file.endsWith('.txt')) {
-                filesList.push(file); 
-            }
+    if (fs.existsSync(__dirname+'/results')) {
+        fs.readdir(__dirname+'/results', function (err, files) {
+            files.forEach(function (file) {
+                console.log(file)
+                if (file.endsWith('.txt')) {
+                    filesList.push(file); 
+                }
+            });
+            req.send(filesList);
         });
-        req.send(filesList);
-    });
+    } else {
+        res.send([]);
+    }
 })
 
-app.get('/clean', (_res, req) => {
-    rimraf(__dirname+'/results', () => req.status(200).send('All results are deleted'));
+app.get('/clean', (_req, res) => {
+    rimraf(__dirname+'/results', () => res.status(200).send('All results are deleted'));
 })
 
 app.listen(port, () => {
