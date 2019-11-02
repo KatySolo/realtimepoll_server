c397eba57c2df714d91b026de8e28d2a056aa62e
Solodovnikova Ekaterina <colo18@yandex.ru>
2019-11-02 20:46:22 +0500

    merging

---
+++ src/types.ts
@@ -0,0 +1,42 @@
+export type SessionType = {
+    id: number,
+    name: string
+}
+
+export type SessionDataType = {
+    lector: LectorResultType,
+    results: ListenersResultType,
+    delta: ListenersResultType,
+    coments: CommentsType,
+    rawResults: PersonDataType[]
+}
+
+export type ResultsType = {
+    [key: string]: PersonDataType[] 
+}
+
+export type LectorResultType = {
+    name: string,
+    form: string,
+    content: string,
+    interest: string;
+}
+
+export type ListenersResultType = {
+    form: number,
+    content: number,
+    interest: number;
+}
+
+export type CommentsType = {
+    [key: string]: string
+}
+
+export type PersonDataType = {
+    name: string,
+    form: string;
+    content: string,
+    interest: string,
+    comment: string,
+    isLector: boolean
+}
