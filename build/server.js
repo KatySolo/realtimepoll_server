18cd16499379fdb224ba02cfe50eefab18a7286a
Solodovnikova Ekaterina <colo18@yandex.ru>
2019-11-02 11:51:21 +0500

    добавлен ts

--- build/server.js
+++ build/server.js
@@ -1,47 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 var cors = require('cors');
 var bodyParser = require("body-parser");
 var rimraf = require('rimraf');
-
 var port = process.env.PORT || 8080;
-
-let sessions = [{id: 17, name: "Арина (Методы атак и защиты веб-приложений)"}, {id: 24, name: "Андрей (Особенности гос. регулирования Интернета)"}];
+let sessions = [{ id: 17, name: "Арина (Методы атак и защиты веб-приложений)" }, { id: 24, name: "Андрей (Особенности гос. регулирования Интернета)" }];
 let sessionsId = [17, 24];
 var currentSessions = [];
-var results = {};
-
+var results = null;
 const testData = [{
-    "name":"Солодовникова Екатерина",
-    "form":"6",
-    "content":"7",
-    "interest":"3",
-    "comment":"Все было очень душевно и качественно",
-    "isLector":true
-},{
-    "name":"Боб Джонстон",
-    "form":"7",
-    "content":"6",
-    "interest":"2",
-    "comment":"Абсолютная чушь!\r\n",
-    "isLector":false
-},{
-    "name":"Марк",
-    "form":"10",
-    "content":"7",
-    "interest":"4",
-    "comment":"Уже лучше\r\n",
-    "isLector":false
-}]
-
+        "name": "Солодовникова Екатерина",
+        "form": "6",
+        "content": "7",
+        "interest": "3",
+        "comment": "Все было очень душевно и качественно",
+        "isLector": true
+    }, {
+        "name": "Боб Джонстон",
+        "form": "7",
+        "content": "6",
+        "interest": "2",
+        "comment": "Абсолютная чушь!\r\n",
+        "isLector": false
+    }, {
+        "name": "Марк",
+        "form": "10",
+        "content": "7",
+        "interest": "4",
+        "comment": "Уже лучше\r\n",
+        "isLector": false
+    }];
 const app = express();
-
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 app.get('/', (_req, res) => {
     const intro = `
     <h2>Сервер для создания и проведения опросов в реальном времени </h2>
@@ -58,155 +54,148 @@ app.get('/', (_req, res) => {
     </ul>
     `;
     res.status(200).send(intro);
-})
-
+});
 app.post('/results', (req, res) => {
-    var {sessionId, form, content, interest, username, comment, lector} = req.body; 
+    var { sessionId, form, content, interest, username, comment, lector } = req.body;
     console.log(req.body);
     try {
         if (currentSessions.indexOf(parseInt(sessionId)) === -1) {
             res.status(403).send('Опрос еще не начался');
-        } else {
+        }
+        else {
             let curResults = results[sessionId];
-            let userResults = {name: username, form, content, interest, comment, isLector: lector === 'on' ? true : false};
+            let userResults = { name: username, form, content, interest, comment, isLector: lector === 'on' ? true : false };
             if (curResults) {
                 results[sessionId].push(userResults);
-            } else {
+            }
+            else {
                 results[sessionId] = [userResults];
             }
             res.status(200).send(`${username}, ваш ответ принят\n`);
         }
-    } catch(err) {
+    }
+    catch (err) {
         res.send('Произошла ошибка на сервере');
     }
-})
-
+});
 app.get('/sessions', (_req, res) => {
     res.send(sessions);
-})
-
+});
 app.post('/start', (req, res) => {
     const sessionId = req.body.id;
     if (sessionsId.indexOf(parseInt(sessionId)) !== -1) {
         currentSessions.push(parseInt(sessionId));
         res.status(200).send(`Начинаем опрос: ${sessionId}\n`);
-    } else {
+    }
+    else {
         res.status(404).send(`Опрос id=${sessionId} не существует.\nСоздайте его POST запросом /add?name=newName\n`);
     }
-})
-
+});
 app.post('/stop', (req, res) => {
     const sessionId = req.body.id;
     if (currentSessions.indexOf(parseInt(sessionId)) === -1) {
         res.status(403).send(`Невозможно остановить опрос, так как он еще не был начат.\n`);
-    } else {
+    }
+    else {
         currentSessions.splice(currentSessions.indexOf(parseInt(sessionId)), 1);
         const result = performCalc(results[sessionId]);
-        fs.writeFile(__dirname+'/results/'+sessionId+".json", JSON.stringify(result), function(err) {
+        fs.writeFile(__dirname + '/results/' + sessionId + ".json", JSON.stringify(result), function (err) {
             if (err) {
-                fs.mkdir(__dirname+'/results', () => {
-                    fs.writeFile(__dirname+'/results/'+sessionId+".json", JSON.stringify(result), () => {})
+                fs.mkdir(__dirname + '/results', () => {
+                    fs.writeFile(__dirname + '/results/' + sessionId + ".json", JSON.stringify(result), () => { });
                 });
             }
         });
         res.status(200).send(`Опрос id=${sessionId} останевлен.\nЧтобы получить результаты, используйте GET запрос /results?id=${sessionId}\n`);
     }
 });
-
 function performCalc(results) {
-
-    if (results === undefined) return {};
-    
-    let lectorResult = {};
-    let listenersResults = {form: 0, content: 0, interest: 0};
-    let comments = {};
-
+    if (results === undefined)
+        return null;
+    let lectorResult = { name: '', form: '', content: '', interest: '' };
+    let listenersResults = { form: 0, content: 0, interest: 0 };
+    let coments = {};
     results.forEach(result => {
         if (result.isLector) {
-            lectorResult = result;
-        } else {
-            listenersResults.form += parseInt(result.form)
+            lectorResult = {
+                name: result.name,
+                form: result.form,
+                content: result.content,
+                interest: result.interest
+            };
+        }
+        else {
+            listenersResults.form += parseInt(result.form);
             listenersResults.content += parseInt(result.content);
             listenersResults.interest += parseInt(result.interest);
         }
-        comments[result.name] = result.comment;
+        coments[result.name] = result.comment;
     });
-
     listenersResults.form = listenersResults.form / (results.length - 1);
     listenersResults.content = listenersResults.content / (results.length - 1);
     listenersResults.interest = listenersResults.interest / (results.length - 1);
-
     const delta = {
-        form: Math.abs(lectorResult.form - listenersResults.form),
-        content: Math.abs(lectorResult.content - listenersResults.content),
-        interest: Math.abs(lectorResult.interest - listenersResults.interest)
-    }
+        form: Math.abs(parseInt(lectorResult.form) - listenersResults.form),
+        content: Math.abs(parseInt(lectorResult.content) - listenersResults.content),
+        interest: Math.abs(parseInt(lectorResult.interest) - listenersResults.interest)
+    };
     return {
         lector: {
-            name: lectorResult.name, 
+            name: lectorResult.name,
             form: lectorResult.form,
             content: lectorResult.content,
             interest: lectorResult.interest
-        }, 
-        results: listenersResults, 
+        },
+        results: listenersResults,
         delta,
-        comments,
+        coments,
         rawResults: results
-    }
-
+    };
 }
-
 app.get('/results', (req, res) => {
-    sessionId = req.query.id;
-    const path = __dirname+'/results/'+sessionId+'.json'; 
-    if (fs.existsSync(path)){
-        res.download(__dirname + '/results/' + sessionId+'.json');
-    } else {
+    let sessionId = req.query.id;
+    const path = __dirname + '/results/' + sessionId + '.json';
+    if (fs.existsSync(path)) {
+        res.download(__dirname + '/results/' + sessionId + '.json');
+    }
+    else {
         res.status(404).send('Опрос еще не завершен.\n');
     }
-})
-
-app.get('/current', (_req,res) => {
-    res.send({id: currentSessions});
-})
-
+});
+app.get('/current', (_req, res) => {
+    res.send({ id: currentSessions });
+});
 app.post('/add', (req, res) => {
     const name = req.body.name;
-    
     let id = Math.ceil(Math.random() * 100);
     while (sessionsId.indexOf(id) !== -1) {
         id = Math.ceil(Math.random() * 100);
     }
-    sessions.push({id, name});
+    sessions.push({ id, name });
     sessionsId.push(id);
-
-    res.status(200).send({id});
-})
-
+    res.status(200).send({ id });
+});
 app.get('/files', (_req, res) => {
     var filesList = [];
-    if (fs.existsSync(__dirname+'/results')) {
-        fs.readdir(__dirname+'/results', function (err, files) {
+    if (fs.existsSync(__dirname + '/results')) {
+        fs.readdir(__dirname + '/results', function (err, files) {
             files.forEach(function (file) {
-                console.log(file)
                 if (file.endsWith('.json')) {
-                    filesList.push(file); 
+                    filesList.push(file);
                 }
             });
             res.send(filesList);
         });
-    } else {
+    }
+    else {
         res.send([]);
     }
-})
-
+});
 app.get('/clean', (_req, res) => {
-    rimraf(__dirname+'/results', () => res.status(200).send('Все результаты удалены.'));
-})
-
+    rimraf(__dirname + '/results', () => res.status(200).send('Все результаты удалены.'));
+});
 app.listen(port, () => {
     console.log(`Приложение запущенно на порту ${port}`);
 });
-
-// curl -d "name=Mарк" -X POST http://localhost:8080/add
 module.exports = app;
+//# sourceMappingURL=server.js.map
